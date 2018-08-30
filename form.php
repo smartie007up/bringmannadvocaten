58d7185609310d320ca6b57d108000b627169f1a
smartie007up <Sinaasappel1>
2018-08-30 17:02:41 +0200

    mob-friendly works

--- form.php
+++ form.php
@@ -1,46 +1,94 @@
 <?php
 
-$headers = "From: PB@" . $_SERVER['SERVER_NAME'] . "\r\n";
+// define variables and set to empty values
+$name_error = $email_error = $message_error = "";
+$name = $email = $gender = $message = $success = "";
 
-//check if form is submitted
-if(empty($_POST['submit']))
-{
-  echo "Het formulier is niet verzonden.";
-  exit;
+//form is submitted with POST method
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  if (empty($_POST["name"])) {
+    $name_error .= "\n Error: Naam is een verplicht veld";
+  } else {
+    $name = test_input($_POST["name"]);
+    // check if name only contains letters and whitespace
+    if (!preg_match("/^[a-zA-Z ]*$/",$name)) {
+      $name_error .= "\n Alleen letters en spaties toegestaan";
+    }
+  }
+
+  if (empty($_POST["email"])) {
+    $email_error .= "\n Error: Email is een verplicht veld";
+  }  else {
+    $email = test_input($_POST["email"]);
+    // check if e-mail address is well-formed
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+      $email_error .= "\n Ongeldige email opmaak";
+    }
+  }
+
+  if (empty($_POST["gender"])) {
+    $gender = "";
+  } else {
+    $gender = test_input($_POST["gender"]);
+  }
+
+  if (empty($_POST["message"])) {
+    $message = "";
+  } else {
+    $message = test_input($_POST["message"]);
+  }
+
+
+
+
+  /*
+  if ($name_error == '' and $email_error == '' and $gender_error == ''){
+  $message_body = '';
+  unset($_POST['submit']);
+  foreach ($_POST as $key => $value){
+  $message_body .=  "$key: $value\n";
 }
 
-// if($_SERVER["REQUEST_METHOD"] == "POST")
-// {
-//   if(empty($_POST['fullname'] ||
-//   empty($_POST['email'])))
-//   {
-//     echo "Vul alstublieft het formulier (volledig) in.";
-//     exit;
-//   }
-//   else {
-//       $name = $_POST["fullname"];
-//       $email = $_POST["email"]
-//       $message = $_POST["message"]
-//       exit;
-//   }
-// }
+$to = 'marthegeman@gmail.com';
+$subject = 'Ingevuld contactformulier';
+if (mail($to, $subject, $message)){
+$success = "Uw bericht is verzonden, bedankt!";
+//echo "Uw bericht is verzonden, bedankt!";
+$name = $email = $gender = $message = '';
 
-if(empty($_POST['fullname'] ||
-  empty($_POST['email'])))
-{
-  echo "Vul alstublieft het formulier (volledig) in.";
-  exit;
+}
+}
+*/
+
+if(isset($_POST['submit'])){
+  if (!empty($name_error == '' || $email_error == '' || $gender_error == '')){
+    unset($_POST['submit']);
+  }
+  else if(empty($name_error) and empty($email_error) and empty($gender_error)){
+    $to = $ontvanger;
+    $onderwerp = "Contactformulier van: $name";
+    $text = "Van: $name \n E-Mail: $email \n Bericht: \n $message";
+
+    if (mail($to,$onderwerp,$text)) {
+      $success = "Uw bericht is verzonden, bedankt!";
+      $name = $email = $gender = $message = '';
+    } else {
+      echo "<p>Something went wrong, go back and try again!</p>";
+    }
+    //redirect to the 'thank you' page
+    //  header('Location: index.html');
+  }
 }
 
-//define variables and set to empty values
-$name = $_POST["fullname"];
-$email = $_POST["email"];
-$message = $_POST["message"];
 
-$to = 'pb@bringmannadvocaten.nl';
-$onderwerp = 'Ingevuld contactformulier';
-$bericht = "\n Van: $name \n \n E-Mailadres: $email \n \n Bericht: $message";
 
-mail($to,$onderwerp,$bericht, "From: $email");
+}
 
-header('Location: thank-you.html');
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
+}
+
+?>
--- java.js
+++ java.js
@@ -13,44 +13,8 @@ $ = function(id) {
 var show = function(id) {
   var pop = $(id);
   pop.style.display ='block';
-
 }
 
 var hide = function(id) {
   $(id).style.display ='none';
 }
-
-document.contactform.onsubmit=function()
-{
-  var emailID = document.contactform.email.value;
-  atpos = emailID.indexOf("@");
-  dotpos = emailID.lastIndexOf(".");
-
-  if(document.contactform.fullname.value=="")
-  {
-    alert("Vul alstublieft uw naam in");
-    document.contactform.fullname.focus();
-    return false;
-  }
-  else if(document.contactform.email.value=="")
-  {
-    alert("Vul alstublieft uw e-mailadres in");
-    document.contactform.email.focus();
-    return false;
-  }
-  else if (atpos < 1 || ( dotpos - atpos < 2 ))
-  {
-    alert("Vul alstublieft een juist emailadres in.")
-     document.contactform.email.focus() ;
-    return false;
-  }
-  else if(document.contactform.message.value=="")
-  {
-    alert("Schrijf alstublieft een bericht zodat ik u beter kan helpen");
-    document.contactform.message.focus();
-    return false;
-  }
-  return true;
-}
-
-//https://www.tutorialspoint.com/javascript/javascript_form_validations.htm
